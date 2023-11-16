32382de4f79fd3e5e9dfc447404f7a5d50394836
Bhargavakkeni <bhargavakkeni123@gmail.com>
2023-11-16 23:29:48 +0530

    updated urls

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 urlpatterns=[
     path('',views.root,name='root'), #url when user enter the app
     path('loginpage/',views.index,name='loginpage'), #url for login
-    path('verifyUser',views.verifyUser,name='verifyUser'), #url for verification
+    path('loginpage/verifyUser',views.verifyUser,name='verifyUser'), #url for verification
     path('registration',views.registration,name='registration'), #url for registration
     path('uploading',views.uploading,name='uploading'),     #url for uplading data to the model
-    path('loginpage/omsadmin',views.omsadmin,name='omsadmin'), #url for displaying a dummy template when login is successful
+    path('omsadmin',views.omsadmin,name='omsadmin'), #url for displaying a dummy template when login is successful
 ]
